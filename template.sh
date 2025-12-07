87d2f2c9da81b23f3a909bfd0fe3f87ec3e99dfc
muhammadharis <muhammadharis.imtiaz1@gmail.com>
2025-12-07 16:04:49 +0100

    Project structure and setup done!

---
+++ template.sh
@@ -0,0 +1,13 @@
+mkdir -p src
+mkdir -p research
+
+touch src/__init.py
+touch src/helper.py
+touch .env
+touch setup.py
+touch app.py
+touch research/notebook_test.ipynb
+touch requirements.txt
+
+echo "All Files and Folders are created sucessfully"
+
